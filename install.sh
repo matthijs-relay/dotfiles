b4aaaceb052f7bb87fae6b93cdae0a9bfc7f7506
Matthijs Wolting <matthijs@collabai.co>
2022-01-26 13:47:22 +0100

    Use an install.sh instead

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+cp $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
