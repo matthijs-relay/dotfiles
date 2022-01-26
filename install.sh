90cc779d3f16bafb6775382a9d595b415062e923
Matthijs Wolting <matthijs@collabai.co>
2022-01-26 13:50:18 +0100

    Moore more fixes

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
