fd74e0341c9e993cbfdd7b49607c58ca76d67e20
Matthijs Wolting <matthijs@collabai.co>
2022-01-26 13:59:36 +0100

    Theme

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+curl -o $HOME/.oh-my-zsh/themes/agnoster-light.zsh-theme https://raw.githubusercontent.com/mkolosick/agnoster-light/master/agnoster-light.zsh-theme
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
