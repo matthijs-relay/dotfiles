4e3ee82fe5826c5a2ae142311f89dbc2c3935b3b
Matthijs Wolting <matthijs@collabai.co>
2022-02-04 13:05:46 +0100

    Fix syntax error

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-cp $HOME/dotfiles/agnoster-light-custom.zsh-theme $HOME/.oh-my-zsh/themes
+cp $HOME/.dotfiles/agnoster-light-custom.zsh-theme $HOME/.oh-my-zsh/themes
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
